42e24bac8ac0a8e3587210d675723ec7787c2ea7
bugger <usernamesarenoteasy@proton.me>
2023-08-19 20:43:07 -0600

    stuff

--- farm.sh
+++ farm.sh
@@ -1,8 +1,11 @@
 while [ 2 -eq 2 ]; do
-	sed -i 's/2/2/' farm.sh
-	sed -i 's/1/1/' farm.sh
+	if [ $(cat "$0" | tail -n 1) == "# 1" ]; then
+		sed -i 's/^# 1/# 2/' farm.sh
+	else
+		sed -i 's/^# 2/# 1/' farm.sh
+	fi;
 
 	git commit -am 'stuff'
 done
 
-2
+# 1
