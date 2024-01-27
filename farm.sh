ff3f32344fb04f886205388d0ff3e072d5984fd3
bugger <usernamesarenoteasy@proton.me>
2024-01-27 00:40:13 -0700

    made it a little more efficient

--- farm.sh
+++ farm.sh
@@ -1,11 +1,3 @@
 while [ 2 -eq 2 ]; do
-	if [ "$(tail -n 1 < farm.sh)" = "1" ]; then
-		sed -i 's/^1/2/' farm.sh
-	else
-		sed -i 's/^2/1/' farm.sh
-	fi;
-
-	git commit -am 'stuff'
+    git commit --allow-empty -m 'stuff'
 done
-
-1
